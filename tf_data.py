cd3c2b146314393db89ffde9fa30b38fb3b7659e
sadegh khosravi <e03skh@gmail.com>
2022-05-27 14:49:38 +0430

    add split teset data , plot history method , plot batch of data

--- tf_data.py
+++ tf_data.py
@@ -3,22 +3,40 @@ import pathlib
 import numpy as  np
 import os
 
+from matplotlib import pyplot as plt
+
 AUTOTUNE = tf.data.experimental.AUTOTUNE
 
 
-class TF_Data:
-    def __init__(self, directory, batch_size, shuffle_buffer_size, augmentation=False, validation_size=0.2):
+class TF_Dataa:
+    def __init__(self, directory,
+                 validation_size=0.2,
+                 test_size=0.3,
+
+                 batch_size=32,
+                 shuffle_buffer_size=1000,
+                 augmentation=False,
+                 shuffle_dataset=True,
+                 input_target_size=[150, 150, 3]
+                 ):
         self.directory = directory
         self.batch_size = batch_size
         self.shuffle_buffer_size = shuffle_buffer_size
         self.augmentation = augmentation
+        self.shuffle_dataset = shuffle_dataset
         self.validation_size = validation_size
+        self.input_target_size = input_target_size
+        self.test_size = test_size
+
+        print(self.input_target_size)
 
     def augment_data(self, image):
-        image = tf.image.resize_with_crop_or_pad(image, 180, 180)
-        image = tf.image.random_crop(image, size=[150, 150, 3])
+        image = tf.image.resize_with_crop_or_pad(image, (self.input_target_size[0] + 30),
+                                                 (self.input_target_size[1] + 30))
+        image = tf.image.random_crop(image, size=[self.input_target_size[0], self.input_target_size[1]])
+        image = tf.image.random_crop(image, size=self.input_target_size)
         image = tf.image.random_brightness(image, max_delta=0.5)
-
+        image = tf.image.random_flip_left_right(image, seed=None)
         return image
 
     def get_classNames(self, ):
@@ -27,23 +45,39 @@ class TF_Data:
         return CLASSNAME
 
     def get_fullDataset(self):
-        full_data = tf.data.Dataset.list_files(str(self.directory / '*/*'))
+        full_data = tf.data.Dataset.list_files(str(self.directory + "*/*"))
+        # if self.shuffle_dataset:
+        #   full_data = tf.data.Dataset.shuffle(self, seed=100, buffer_size = AUTOTUNE)
         return full_data
 
     def getTrainValidationData(self, ):
-        dataset_size = len(list(self.get_fullDataset()))
+        full_dataset = self.get_fullDataset()
 
-        train_size = int((1 - self.validation_size) * dataset_size)
-        train_dataset = self.get_fullDataset().take(train_size)
-        validation_dataset = self.get_fullDataset().skip(train_size)
+        DATASET_SIZE = len(list(full_dataset))
+        print("full datast size is: {} ".format(DATASET_SIZE))
 
-        return train_dataset, validation_dataset
+        train_size = int((1 - (self.validation_size + self.test_size)) * DATASET_SIZE)
+        test_size = int(self.test_size * DATASET_SIZE)
+        val_size = int(self.validation_size * DATASET_SIZE)
+
+        train_dataset = full_dataset.take(train_size)
+        remaining = full_dataset.skip(train_size)
+
+        test_dataset = remaining.take(test_size)
+        val_dataset = remaining.skip(test_size)
+
+        print("train size: {}, validation size:{} , test size ={}".format(
+            len(list(train_dataset)), len(list(val_dataset)), len(list(test_dataset))
+        ))
+
+        return train_dataset, val_dataset, test_dataset
 
     def get_label(self, file_path):
         parts = tf.strings.split(file_path, os.path.sep)
         return parts[-2] == self.get_classNames()
 
     def load_image(self, image_path):
+        print("********* getTrainValidationData *************** ", len(self.input_target_size))
         img = tf.io.read_file(image_path)
         img = tf.image.decode_image(img, 3, expand_animations=False)
         img = tf.cast(img, tf.float32)
@@ -62,35 +96,73 @@ class TF_Data:
         return image
 
     def load_image_with_label(self, image_path):
+        print("********* load_image_with_label ***************")
+
         lable = self.get_label(image_path)
         image = self.load_image(image_path)
 
         return image, lable
 
     def load_image_train(self, image_path):
+        print("********* load_image_train ***************")
+
         image, lable = self.load_image_with_label(image_path)
         if self.augmentation:
             image = self.augment_data(image)
+
+        image = self.resize_image(image, self.input_target_size[0],
+                                  self.input_target_size[1], )
         image = self.normalize(image)
 
         return image, lable
 
     def load_image_test(self, image_path):
-        image, lable = self.load_image_with_label(image_path)
-        image = self.resize_image(image, 150, 150)
+        print("********* load_image_test ***************")
+
+        image, label = self.load_image_with_label(image_path)
+        image = self.resize_image(image, self.input_target_size[0],
+                                  self.input_target_size[1], )
         image = self.normalize(image)
-        return image, lable
+        return image, label
 
     def train_validation_split(self):
-        train_dataset, validation_dataset = self.getTrainValidationData()
+        print("********* train_validation_split ***************")
+        train_dataset, validation_dataset, test_dataset = self.getTrainValidationData()
 
-        train_dataset = train_dataset.map(self.load_image_train, num_parallel_calls=AUTOTUNE)
+        train_dataset = train_dataset.map(self.load_image_train)
         train_dataset = train_dataset.shuffle(self.shuffle_buffer_size)
         train_dataset = train_dataset.batch(self.batch_size)
-        train_dataset = train_dataset.prefetch(buffer_size=AUTOTUNE)
+        train_dataset = train_dataset.prefetch(AUTOTUNE)
 
-        validation_dataset = validation_dataset.map(self.load_image_test, num_parallel_calls=AUTOTUNE)
-        validation_dataset = validation_dataset.cache()
+        validation_dataset = validation_dataset.map(self.load_image_test)
         validation_dataset = validation_dataset.batch(self.batch_size)
+        validation_dataset = validation_dataset.prefetch(AUTOTUNE)
 
-        return train_dataset, validation_dataset
+        test_dataset = test_dataset.map(self.load_image_test)
+        test_dataset = test_dataset.batch(self.batch_size)
+        test_dataset = test_dataset.prefetch(AUTOTUNE)
+
+        return train_dataset, validation_dataset, test_dataset
+
+
+    def plot_batch_data(self,dataset):
+        image_batch, label_batch = next(iter(dataset))
+        plt.figure(figsize=(10, 10))
+        for i in range(25):
+            ax = plt.subplot(5, 5, i + 1)
+            plt.imshow(image_batch[i])
+            plt.title(label_batch[i].numpy())
+            plt.axis("off")
+
+    def plot_hist(self, hist):
+        plt.plot(hist.history["accuracy"])
+        plt.plot(hist.history["val_accuracy"])
+        plt.plot(hist.history["loss"])
+        plt.plot(hist.history["val_loss"])
+        plt.title("Training Progress")
+        plt.ylabel("Accuracy/Loss")
+        plt.xlabel("Epochs")
+        plt.legend(["train_acc", "val_acc", "train_loss", "val_loss"], loc="upper left")
+        plt.show()
+
+
